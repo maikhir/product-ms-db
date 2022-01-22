e8ae47f370769fe6f4fb555df3944b3fd4ec44df
Maik Hirthe <Maik.Hirthe@rohde-schwarz.com>
2022-01-22 16:58:59 +0100

    add Eureka Client + 0.0.2

--- src/main/java/de/hirthe/ms/productms/ProductMsApplication.java
+++ src/main/java/de/hirthe/ms/productms/ProductMsApplication.java
@@ -2,9 +2,11 @@ package de.hirthe.ms.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductMsApplication {
 
 	public static void main(String[] args) {
