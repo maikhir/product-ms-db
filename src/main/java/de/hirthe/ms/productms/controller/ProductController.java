37c40df996eb27ab17ec858847c76d49e65bfd5a
Maik Hirthe <Maik.Hirthe@rohde-schwarz.com>
2022-02-12 15:49:21 +0100

    refresh of git config work!

--- src/main/java/de/hirthe/ms/productms/controller/ProductController.java
+++ src/main/java/de/hirthe/ms/productms/controller/ProductController.java
@@ -4,6 +4,7 @@ import de.hirthe.ms.productms.model.Product;
 import de.hirthe.ms.productms.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +12,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/product")
+@RefreshScope
 public class ProductController {
 
     @Autowired
     private IProductService productService;
 
-    @Value("test.name")
+    @Value("${test.name}")
     private String testName;
 
     @GetMapping("/name") //api/product/name
-    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    @ResponseStatus(HttpStatus.OK)
     public String getName() {
         return testName;
     }
